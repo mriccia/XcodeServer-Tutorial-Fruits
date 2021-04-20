18cd8f9274775008cf0a1dacea1bb9f70031c310
Michele Ricciardi <mriccia@amazon.com>
2021-04-20 13:46:30 +0800

    use correct env vars for App store Connect API

--- runscript.sh
+++ runscript.sh
@@ -6,7 +6,9 @@ export APPLE_TEAM_ID=$(aws secretsmanager get-secret-value --secret-id APPLE_TEA
 export MATCH_S3_BUCKET=$(aws secretsmanager get-secret-value --secret-id MATCH_S3_BUCKET --query SecretString --output text)
 export APP_STORE_CONNECT_API_KEY_KEY_ID=$(aws secretsmanager get-secret-value --secret-id APP_STORE_CONNECT_API_KEY_KEY_ID --query SecretString --output text)
 export APP_STORE_CONNECT_API_KEY_ISSUER_ID=$(aws secretsmanager get-secret-value --secret-id APP_STORE_CONNECT_API_KEY_ISSUER_ID --query SecretString --output text)
-export APP_STORE_CONNECT_API_KEY_KEY_CONTENT=$(aws secretsmanager get-secret-value --secret-id APP_STORE_CONNECT_API_KEY_KEY_CONTENT --query SecretString --output text)
+export APP_STORE_CONNECT_API_KEY_KEY=$(aws secretsmanager get-secret-value --secret-id APP_STORE_CONNECT_API_KEY_KEY_CONTENT --query SecretBinary --output text)
+export APP_STORE_CONNECT_API_KEY_IS_KEY_CONTENT_BASE64=true
+
 export MATCH_PASSWORD=$(aws secretsmanager get-secret-value --secret-id MATCH_PASSWORD --query SecretString --output text)
 export MATCH_KEYCHAIN_PASSWORD=$(aws secretsmanager get-secret-value --secret-id MATCH_PASSWORD --query SecretString --output text)
 export AWS_REGION="us-east-2"
