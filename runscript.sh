794a81e610f89a0ff7e7f904c3808f10f23419e4
Michele Ricciardi <mriccia@amazon.com>
2021-04-20 12:10:33 +0800

    rename env var to use App Store Connect API instead

--- runscript.sh
+++ runscript.sh
@@ -1,7 +1,7 @@
 set +x
 source ~/.zshrc
 
-export APPLE_ID=$(aws secretsmanager get-secret-value --secret-id APPLE_ID --query SecretString --output text)
+export T_APPLE_ID=$(aws secretsmanager get-secret-value --secret-id APPLE_ID --query SecretString --output text)
 export APPLE_TEAM_ID=$(aws secretsmanager get-secret-value --secret-id APPLE_TEAM_ID --query SecretString --output text)
 export MATCH_S3_BUCKET=$(aws secretsmanager get-secret-value --secret-id MATCH_S3_BUCKET --query SecretString --output text)
 export APP_STORE_CONNECT_API_KEY_KEY_ID=$(aws secretsmanager get-secret-value --secret-id APP_STORE_CONNECT_API_KEY_KEY_ID --query SecretString --output text)
@@ -9,8 +9,9 @@ export APP_STORE_CONNECT_API_KEY_ISSUER_ID=$(aws secretsmanager get-secret-value
 export APP_STORE_CONNECT_API_KEY_KEY_CONTENT=$(aws secretsmanager get-secret-value --secret-id APP_STORE_CONNECT_API_KEY_KEY_CONTENT --query SecretString --output text)
 export MATCH_PASSWORD=$(aws secretsmanager get-secret-value --secret-id MATCH_PASSWORD --query SecretString --output text)
 export MATCH_KEYCHAIN_PASSWORD=$(aws secretsmanager get-secret-value --secret-id MATCH_PASSWORD --query SecretString --output text)
-export FASTLANE_PASSWORD=$(aws secretsmanager get-secret-value --secret-id FASTLANE_PASSWORD --query SecretString --output text)
-export FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD=$(aws secretsmanager get-secret-value --secret-id FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD --query SecretString --output text)
+export AWS_REGION="us-east-2"
 
 # Build
+fastlane tests
+fastlane build_ci
 fastlane beta_ci
